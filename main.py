7fc265a76b2adeb55e39db9fedde2439823d4c08
Bruno <141152582+BrUn0-P1nh41r0@users.noreply.github.com>
2024-11-23 17:36:27 +0000

    Another exercise from day 16

--- main.py
+++ main.py
@@ -2,15 +2,31 @@
 #print(another_module.another_variable)
 #
 
-from turtle import Turtle, Screen
+#from turtle import Turtle, Screen
 
-timmy = Turtle()
-print(timmy)
-timmy.shape("turtle")
-timmy.color("Coral")
+#timmy = Turtle()
+#print(timmy)
+#timmy.shape("turtle")
+#timmy.color("Coral")
 
-timmy.forward(100)
+#timmy.forward(100)
 
-my_screen = Screen()
-print(my_screen.canvheight)
-my_screen.exitonclick()
+#my_screen = Screen()
+#print(my_screen.canvheight)
+#my_screen.exitonclick()
+
+from prettytable import PrettyTable
+
+table = PrettyTable()
+
+table.field_names = ["Pokemon Name", "Type"]
+table.add_rows(
+    [
+        ["Pikachu", "Electric"],
+        ["Squirtle", "Water"],
+        ["Charmander", "Fire"],
+    ]
+)
+
+print (table)
+
